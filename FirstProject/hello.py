f09e5534884eb16765d2c57df22fcb1448d125a7
BRIJESH MISHRA <brijeshmishra27@gmail.com>
2026-07-19 23:57:30 +0530

    chage code

--- FirstProject/hello.py
+++ FirstProject/hello.py
@@ -1,5 +1,1 @@
-print("Hello World ")
-print("I am going to be switch my job")
-'''print("this sis the mrrrrmr\nttttttb")'''
-print("I am going to pruchase \"car\" this year")
-print("ram",6,7,sep="|")
+print("Hello GitHub!")
