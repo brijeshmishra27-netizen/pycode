0c4b9867229a198022c1cde600eb6f2d2468e0f2
BRIJESH MISHRA <brijeshmishra27@gmail.com>
2026-07-19 23:49:58 +0530

    Initial commit

---
+++ FirstProject/hello.py
@@ -0,0 +1,5 @@
+print("Hello World ")
+print("I am going to be switch my job")
+'''print("this sis the mrrrrmr\nttttttb")'''
+print("I am going to pruchase \"car\" this year")
+print("ram",6,7,sep="|")
